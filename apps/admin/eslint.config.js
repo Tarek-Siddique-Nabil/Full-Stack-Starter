986e689d89ac5ed36301e4b066cb286d5f3d97a6
Tarek Siddique Nabil <nabilh420@gmail.com>
2024-12-17 12:41:29 +0600

    initialize project

---
+++ apps/admin/eslint.config.js
@@ -0,0 +1,6 @@
+// Purpose: ESLint configuration for Vite projects.
+import { viteJsConfig } from "@repo/eslint-config/vite.js";
+
+/**   @type {import("eslint").Linter.Config} */
+
+export default viteJsConfig;
